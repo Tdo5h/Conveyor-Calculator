8b687b5a6eb64b7f8770f6ef0b6b88797fba32ca
Tdo5h <144173472+Tdo5h@users.noreply.github.com>
2023-09-17 05:57:41 +1200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,14 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('conveyor-calculator-v1').then((cache) => {
-        return cache.addAll([
-          './index.html',
-          './icon.png',
-          // Add other assets you want to cache here
-        ]);
-      })
-    );
-  });
-  
+
   self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.respondWith(
+    caches.open('conveyor-calculator-v1').then((cache) => {
+      return cache.match(event.request).then((response) => {
+        const fetchPromise = fetch(event.request).then((networkResponse) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+        return response || fetchPromise;
+      });
+    })
+  );
+});
